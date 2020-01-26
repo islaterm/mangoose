155f48869e079788894348302aca9d8183245b02
Ignacio Slater Muñoz <ignacio.slater@ug.uchile.cl>
2020-01-26 16:32:56 -0300

    Started implementation of search v1.1.1.5

--- scrappers/manga_plus.py
+++ scrappers/manga_plus.py
@@ -1,61 +1,11 @@
 """
 Scraper for MANGA Plus series.
 """
-from typing import Dict
-
-import yaml
 
 __author__ = 'Ignacio Slater Muñoz'
 __project__ = "Mangoose"
 __email__ = "islaterm@gmail.com"
-__version__ = "1.1.1.4"
-
-
-class MangooseDatabase:
-    """
-    This class represents the manga database of the downloaded series and chapters.
-    """
-    _series: Dict[str, str]
-
-    def __init__(self):
-        """
-        Creates an empty database.
-        """
-        self._series = {}
-
-    def add_series(self, name: str, link: str) -> 'MangooseDatabase':
-        """
-        Adds a new series to the database with a link to the webpage containing the
-        chapters.
-        :return: the updated database.
-        """
-        self._series[name] = link
-        return self
-
-    def get_series_link(self, param):
-        pass
-
-    def remove(self, ONE_PIECE_TITLE):
-        pass
-
-    def is_empty(self):
-        """
-        :return: True if the database is empty, false otherwise.
-        """
-        return self.size == 0
-
-    def contains(self, series_name: str):
-        """
-        :return: True if the database contains the series, False otherwise.
-        """
-        return series_name in self._series
-
-    @property
-    def size(self) -> int:
-        """
-        :return: the number of entries in the database.
-        """
-        return len(self._series)
+__version__ = "1.1.2.1"
 
 
 class MangaPlusScrapper:
@@ -63,8 +13,4 @@ class MangaPlusScrapper:
     This class contains the functionality to scrape, parse and download manga chapters
     from MANGA Plus.
     """
-    _database: MangooseDatabase
 
-    def __init__(self, database_path: str):
-        with open(database_path, "r+") as db_file:
-            self._database = yaml.load(db_file, Loader=yaml.FullLoader)
--- test/scrappers/test_manga_plus.py
+++ test/scrappers/test_manga_plus.py
@@ -1,9 +1,6 @@
 """
 Test set for the MANGA Plus scrapper.
 """
-from unittest import TestCase
-
-from scrappers.manga_plus import MangooseDatabase
 
 __author__ = 'Ignacio Slater Muñoz'
 __project__ = "Mangoose"
@@ -19,46 +16,3 @@ ONE_PIECE_LINK = "https://mangaplus.shueisha.co.jp/titles/100020"
 BORUTO_LINK = "https://mangaplus.shueisha.co.jp/titles/100006"
 DRAGON_BALL_SUPER_LINK = "https://mangaplus.shueisha.co.jp/titles/200025"
 
-
-class TestMangooseDatabase(TestCase):
-    _test_database: MangooseDatabase
-
-    def setUp(self) -> None:
-        """
-        Initializes the test database.
-        """
-        self._test_database = MangooseDatabase()
-
-    def test_constructor(self):
-        assert self._test_database.is_empty()
-
-    def test_database_operations(self):
-        assert self._test_database.is_empty()
-        self._populate_db()
-        self._test_add()
-        self._test_remove()
-
-    def _populate_db(self):
-        self._test_database \
-            .add_series(ONE_PIECE_TITLE, ONE_PIECE_LINK) \
-            .add_series(BORUTO_TITLE, BORUTO_LINK) \
-            .add_series(DRAGON_BALL_SUPER_TITLE, DRAGON_BALL_SUPER_LINK)
-
-    def _test_add(self):
-        assert self._test_database.size == 3, \
-            f"Expected: 3, Actual: {self._test_database.size}"
-        assert self._test_database.contains(ONE_PIECE_TITLE)
-        assert self._test_database.get_series_link(ONE_PIECE_TITLE) == ONE_PIECE_LINK
-        assert self._test_database.get_series_link(BORUTO_TITLE) == BORUTO_LINK
-        assert self._test_database.get_series_link(
-            DRAGON_BALL_SUPER_TITLE) == DRAGON_BALL_SUPER_LINK
-
-    def _test_remove(self):
-        self._test_database.remove(ONE_PIECE_TITLE)
-        assert self._test_database.size == 2
-        assert not self._test_database.contains(ONE_PIECE_TITLE)
-        self._test_database.remove(ONE_PIECE_TITLE)
-        assert self._test_database.size == 2
-        self._test_database.remove(BORUTO_TITLE)
-        self._test_database.remove(DRAGON_BALL_SUPER_TITLE)
-        assert self._test_database.is_empty()
