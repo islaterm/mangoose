f1a61192a07db45400f00574e9d1d11d1fe3dff8
SL8R <islaterm@gmail.com>
2020-07-09 15:52:12 -0400

    B1: Added basic tests for db operations
    Took 30 minutes

---
+++ test/scrappers/test_manga_plus.py
@@ -0,0 +1,57 @@
+import unittest
+from typing import Dict
+
+import pytest
+
+from scrappers.manga_plus import MangooseDatabase
+
+__version__ = "2.0.0-b.1"
+
+def test_constructor(database: MangooseDatabase):
+    assert database.is_empty()
+
+
+def test_database_operations(database: MangooseDatabase, mangas: Dict[str, str]) -> None:
+    assert database.is_empty()
+    populate_db(database, mangas)
+    check_add(database, mangas)
+    check_remove(database, mangas)
+
+
+def populate_db(database: MangooseDatabase, mangas: Dict[str, str]):
+    for title, link in mangas.items():
+        database.add_series(title, link)
+
+
+def check_add(database: MangooseDatabase, mangas: Dict[str, str]):
+    assert len(database) == 0
+    for title, link in mangas.items():
+        assert database.contains(title)
+        assert database.get_series_link(title) == link
+
+
+def check_remove(database: MangooseDatabase, mangas: Dict[str, str]):
+    expected_size = len(mangas)
+    assert len(database) == expected_size
+    for title, _ in mangas.items():
+        database.remove(title)
+        expected_size -= 1
+        assert len(database) == expected_size
+        assert not database.contains(title)
+    assert database.is_empty()
+
+
+@pytest.fixture
+def database() -> MangooseDatabase:
+    return MangooseDatabase()
+
+
+@pytest.fixture()
+def mangas() -> Dict[str, str]:
+    return { "Boruto: Naruto Next Generations": "https://mangaplus.shueisha.co.jp/titles/100020",
+             "One Piece": "https://mangaplus.shueisha.co.jp/titles/100006",
+             "Dragon Ball Super": "https://mangaplus.shueisha.co.jp/titles/200025" }
+
+
+if __name__ == '__main__':
+    unittest.main()
